e82c7b9be46a8b5c942df5a9329ae52a9ca15fcb
wanmaoor <wanmao1284879375@outlook.com>
2020-03-19 17:33:20 +0800

    完成路况卡片基本样式

--- Views/Traffic/Traffic.tsx
+++ Views/Traffic/Traffic.tsx
@@ -1,17 +1,38 @@
 import React from 'react';
 import {RouteComponentProps} from 'react-router-native';
-import {Text, View} from 'react-native';
-const Traffic: React.FC<RouteComponentProps> = ({history}) => {
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {WingBlank, Card, WhiteSpace} from '@ant-design/react-native';
+
+const Traffic: React.FC<RouteComponentProps> = () => {
   return (
-    <View style={{flex: 1, backgroundColor: 'blue'}}>
-      <Text
-        onPress={() => {
-          history.push('/login');
-        }}>
-        路况
-      </Text>
-    </View>
+    <ScrollView style={TrafficStyle.sv}>
+      <WhiteSpace size={'xl'} />
+      <WingBlank size="lg">
+        <Card>
+          <Card.Header
+            title={<Text style={TrafficStyle.cardText}>117路</Text>}
+            extra="往xxx方向"
+          />
+          <Card.Body>
+            <View style={{height: 42}}>
+              <Text style={{marginLeft: 16}}>Card Content</Text>
+            </View>
+          </Card.Body>
+          <Card.Footer content="准载人数: 32" />
+        </Card>
+      </WingBlank>
+    </ScrollView>
   );
 };
+
+const TrafficStyle = StyleSheet.create({
+  sv: {
+    flex: 1,
+  },
+  cardText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
 
 export default Traffic;
