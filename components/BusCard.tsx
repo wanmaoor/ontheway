bd3baf4f36a8f8f8bc9d02394e34593469a73277
wanmaoor <wanmao1284879375@outlook.com>
2020-03-22 11:24:05 +0800

    抽离BusCard组件的样式

--- components/BusCard.tsx
+++ components/BusCard.tsx
@@ -19,7 +19,7 @@ const BusCard: React.FC<IBusCardProps> = ({busName, payload, maxPayload}) => {
     ['#229954', '不挤'],
     ['#F1C40F', '拥挤'],
   ];
-  let fillColor;
+  let fillColor: string;
   let statusText;
   if (divide > 0 && divide <= 0.4) {
     fillColor = colors[1][0];
@@ -41,6 +41,7 @@ const BusCard: React.FC<IBusCardProps> = ({busName, payload, maxPayload}) => {
       };
     }
   }, [amount, width]);
+
   return (
     <WingBlank size="lg">
       <WhiteSpace size={'lg'} />
@@ -59,7 +60,7 @@ const BusCard: React.FC<IBusCardProps> = ({busName, payload, maxPayload}) => {
                 height="60"
                 strokeWidth={1}
                 stroke={'#CCD1D1'}
-                rx={8}
+                rx={5}
               />
               <Rect
                 x="15"
@@ -67,16 +68,9 @@ const BusCard: React.FC<IBusCardProps> = ({busName, payload, maxPayload}) => {
                 width={amount}
                 height="60"
                 fill={fillColor}
-                rx={8}
+                rx={5}
               />
-              <Text
-                style={{
-                  marginLeft: '83%',
-                  marginTop: '6%',
-                  fontWeight: 'bold',
-                  fontSize: 18,
-                  color: fillColor,
-                }}>
+              <Text style={[BusCardStyle.SVGText, {color: fillColor}]}>
                 {statusText}
               </Text>
             </Svg>
@@ -93,6 +87,12 @@ const BusCardStyle = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     color: '#17202A',
+  },
+  SVGText: {
+    marginLeft: '83%',
+    marginTop: '6%',
+    fontWeight: 'bold',
+    fontSize: 18,
   },
 });
 
