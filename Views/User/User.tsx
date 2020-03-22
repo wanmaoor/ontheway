ccd70829918b84334327ab69c01e9348c462721a
wanmaoor <wanmao1284879375@outlook.com>
2020-03-22 17:56:55 +0800

    抽离 User 信息面板样式

--- Views/User/User.tsx
+++ Views/User/User.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {RouteComponentProps} from 'react-router-native';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import {Card, WhiteSpace, WingBlank} from '@ant-design/react-native';
 import UserData from '../../components/UserData';
 
@@ -12,11 +12,7 @@ const User: React.FC<IUser> = ({username}) => {
   return (
     <Card full={true}>
       <Card.Header
-        title={
-          <Text style={{fontWeight: 'bold', fontSize: 24, marginTop: 15}}>
-            {username}
-          </Text>
-        }
+        title={<Text style={UserStyles.title}>{username}</Text>}
         thumbStyle={{
           width: 60,
           height: 60,
@@ -26,9 +22,18 @@ const User: React.FC<IUser> = ({username}) => {
         }}
         thumb="https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg"
       />
-      <Card.Footer content={<UserData />} style={{marginBottom: 10}} />
+      <Card.Footer content={<UserData />} style={UserStyles.footer} />
     </Card>
   );
 };
+
+const UserStyles = StyleSheet.create({
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginTop: 15,
+  },
+  footer: {marginBottom: 10},
+});
 
 export default User;
